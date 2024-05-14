d4d89646512ecb692193914cc86283d8bc60e6a7
RedStar Technologies <system@rsthn.com>
2024-05-13 23:33:29 -0600

    v2.0.2

--- README.md
+++ README.md
@@ -1,55 +1,57 @@
 # Rose CLI Utility
 
-Welcome! This repository contains the project to setup a rose-core command line interface to run your `.fn` files right from your console.
-
-<br/>
+This repository contains a composer project to setup a rose-core command line interface to run your `.fn` files, create projects, and more stuff! Right from the comfort of your terminal.
 
 ## Installation
 
 First and foremost, ensure you have PHP 8.1+ and Composer 2+ installed in your system and that those are reachable via PATH.
 
-Open a console and navigate to the folder where you want `rose-cli` to be installed and execute the following commands:
+Open a terminal and navigate to the folder where you want `rose-cli` to be installed and execute the following commands:
 ```sh
 composer create-project rsthn/rose-cli
 cd rose-cli
 composer update
-./install.sh
+./install
 ```
 
 After that, the command `rose` should be globally accessible.
-
-<br/>
 
 ## Removal
 
 Automatic removal is currently not supported, but to remove it manually simply execute `where rose` / `whereis rose` / `which rose` to locate the `rose.sh` or `rose.bat` file that was created in the PHP folder and remove it. Then remove the entire `rose-cli` folder and that is all.
 
-<br/>
-
 ## Commant Line Interface
 
 <br/>
 
-Runs the specified file. Arguments are passed via the global variable. `args`.
+Create a new API project in the specified folder.
 ```sh
-rose <fn-file> [arguments...]
+rose new [target-folder]
+```
+
+Runs the specified file.
+```sh
+rose example.fn [args...]
+```
+
+Executes the given code immediately.
+```sh
+rose -i "(echo 'Hello')"
+```
+
+Throws an error if the operator returns `false` when evaluated with the rose-core version.
+```sh
+rose version ge? 1.0.0
 ```
 
 Shows the rose-core and CLI versions.
 ```sh
 rose version
-rose --version
-rose -v
-```
-
-Execute code from a string.
-```sh
-rose -i "(echo (upper 'Hello World!'))"
 ```
 
 Shows a list of all installed packages.
 ```sh
-rose list
+rose ls
 ```
 
 Installs a package using composer.
@@ -59,18 +61,40 @@ rose add <package-name>
 
 Removes a package.
 ```sh
-rose remove <package-name>
 rose rm <package-name>
 ```
 
-Updates all installed packages.
+Updates all packages or an specific package.
 ```sh
-rose update
-rose up
+rose up [package-name]
 ```
 
-Updates an specific package.
+Pulls a repository of CLI modules in the module cache. If no repository is specified, the default one ([rose-cli-mods](https://github.com/rsthn/rose-cli-mods)) will be used
 ```sh
-rose update <package-name>
-rose up <package-name>
+rose mod-pull [repo-path|repo-url]
+```
+
+Shows all available modules in the module cache.
+```sh
+rose mod-search
+```
+
+Shows a list of installed modules.
+```sh
+rose mod-ls
+```
+
+Installs a module.
+```sh
+rose mod-add <mod-name>
+```
+
+Removes a module.
+```sh
+rose mod-rm <mod-name>
+```
+
+Executes a module with the specified parameters.
+```sh
+rose :mod-name [args..]
 ```
--- cli.php
+++ cli.php
@@ -35,26 +35,32 @@ while ($dir)
 }
 
 Main::cli(Path::dirname(__FILE__));
+Wind::$data = new Map();
 
 set_error_handler ('cli_error_handler', E_STRICT | E_USER_ERROR | E_WARNING | E_USER_WARNING);
 
 $args = new Arry($argv);
 if ($args->length < 2)
 {
-    echo "Use:\n";
-    echo "    rose new [<target-folder>]                 Creates a new API project in the specified folder.\n";
     echo "\n";
-    echo "    rose <fn-file> [arguments...]              Runs the specified file.\n";
-    echo "    rose -i <string>                           Executes the given string immediately.\n";
-    echo "    rose version|--version|-v                  Shows the rose-core and CLI versions.\n";
-    echo "    rose list                                  Shows a list of all installed packages.\n";
-    echo "    rose add <package-name>                    Installs a package using composer.\n";
-    echo "    rose remove|rm <package-name>              Removes a package.\n";
-    echo "    rose update|up [<package-name>]            Updates all packages or an specific package.\n";
-    echo "    rose get <repo-url> [<mod-name>]           Installs a module from a repository.\n";
-    echo "    rose del <mod-name>                        Removes a module.\n";
-    echo "    rose :mod-name [args..]                    Executes a module.\n";
+    echo "  rose new [target-folder]                  Creates a new API project in the specified folder.\n";
     echo "\n";
+    echo "  rose <fn-file> [args...]                  Runs the specified file.\n";
+    echo "  rose -i <code>                            Executes the given code immediately.\n";
+    echo "  rose version op target-version            Checks if the rose-core version matches the condition.\n";
+    echo "  rose version                              Shows the rose-core and CLI versions.\n";
+    echo "\n";
+    echo "  rose ls                                   Shows a list of all installed packages.\n";
+    echo "  rose add <package-name>                   Installs a package using composer.\n";
+    echo "  rose rm <package-name>                    Removes a package.\n";
+    echo "  rose up [package-name]                    Updates all packages or an specific package.\n";
+    echo "\n";
+    echo "  rose mod-pull [repo-path|repo-url]        Pulls a repository of CLI modules in the module cache.\n";
+    echo "  rose mod-search                           Shows all available modules in the module cache.\n";
+    echo "  rose mod-ls                               Shows a list of installed modules.\n";
+    echo "  rose mod-add <mod-name>                   Installs a module.\n";
+    echo "  rose mod-rm <mod-name>                    Removes a module.\n";
+    echo "  rose :mod-name [args..]                   Executes a module with the specified parameters.\n";
     return;
 }
 
@@ -62,163 +68,254 @@ try {
 
     switch($args->get(1))
     {
-        case '--version':
-        case '-v':
+        /**
+         * `version`
+         * `version op value`
+         */
         case 'version':
-            echo "\x1B[97mcore:\x1B[0m v".Main::version()."\n";
-            echo "\x1B[97mcli:\x1B[0m v".(file_get_contents(dirname(__FILE__).'/VERSION.txt'))."\n";
+            if ($args->length > 2)
+            {
+                if (!$args->{3})
+                    throw new Error("Parameter \e[97mtarget-version\e[0m is missing.");
+
+                $tmp = "(def-fn version value ".
+                         "(reduce val in (split '.' (value)) ".
+                            "(+ (* 1000 (s)) (val))".
+                         ")".
+                       ")".
+                       "(".$args->get(2)." '".Main::version()."' '".$args->get(3)."')"
+                    ;
+
+                if (!Expr::eval($tmp, Wind::$data))
+                    throw new Error("Version check failed: \e[97m" . Main::version() . " " . $args->get(2) . " " . $args->get(3) . "\e[0m");
+                break;
+            }
+
+            echo "\n  \e[90mcore:\e[0m \e[97mv".Main::version() . "\e[0m";
+            echo "\n  \e[90mcli:\e[0m v".(json_decode(file_get_contents(dirname(__FILE__).'/composer.json'))->version) . "\e[0m";
             break;
 
+        /**
+         * `-i <code>`
+         */
         case '-i':
             if (!$args->{2})
-                throw new Error('Parameter <string> is missing.');
+                throw new Error("Parameter \e[97mcode\e[0m is missing.");
 
             try {
-                Wind::$data = new Map();
                 $response = Expr::eval($args->{2}, Wind::$data);
-    
                 if ($response != null)
                     Wind::reply ($response);
             }
-            catch (SubReturn $e)
-            {
+            catch (SubReturn $e) {
                 if (!Wind::$contentFlushed)
                     echo Wind::$response;
             }
             catch (FalseError $e) {
             }
-    
             break;
 
-        case 'list':
-            echo "Installed packages:\n\n";
+        /**
+         * `ls`
+         */
+        case 'ls':
             foreach (json_decode(file_get_contents(dirname(__FILE__).'/composer.lock'))->packages as $package)
-                echo "    \x1B[97m".$package->name.":\x1B[0m \x1B[92m".$package->version."\x1B[0m\n";
-
+                echo "\n  \e[90m".$package->name.":\e[0m \e[97m".$package->version."\e[0m";
             break;
 
+        /**
+         * `add <package-name>`
+         */
         case 'add':
             if (!$args->{2})
-                throw new Error('Parameter <package-name> is missing.');
+                throw new Error("Parameter \e[97mpackage-name\e[0m is missing.");
 
             Path::chdir(Path::fsroot());
-
             system ("composer --ansi require " . $args->get(2));
             break;
 
-        case 'remove':
+        /**
+         * `rm <package-name>`
+         */
         case 'rm':
             if (!$args->{2})
-                throw new Error('Parameter <package-name> is missing.');
+                throw new Error("Parameter \e[97mpackage-name\e[0m is missing.");
 
             Path::chdir(Path::fsroot());
-
             system ("composer --ansi remove " . $args->get(2));
             break;
 
-        case 'update':
+        /**
+         * `up [package-name]`
+         */
         case 'up':
             Path::chdir(Path::fsroot());
-
-            if (!$args->{2})
-                system ("composer --ansi update");
-            else
+            if (!!$args->{2})
                 system ("composer --ansi update " . $args->get(2));
-
+            else
+                system ("composer --ansi update");
             break;
 
-        case 'get':
-            if (!$args->{2})
-                throw new Error('Parameter <repo-url> is missing.');
+        /**
+         * `mod-ls`
+         */
+        case 'mod-ls':
+            $tmp = Path::append(Path::fsroot(), 'mods');
+            if (!Path::exists($tmp)) break;
 
-            $tmp = Path::append(Path::fsroot(), 'tmp');
+            foreach (Directory::readDirs($tmp, false)->dirs->__nativeArray as $dir)
+                echo "\n  \e[90m".$dir->name.":\e[0m \e[97m". json_decode(file_get_contents(Path::append($dir->path, 'meta.json')))->version . "\e[0m";
+            break;
 
-            $url = $args->{2};
+        /**
+         * `mod-pull [repo-url]`
+         */
+        case 'mod-pull':
+            $url = $args->{2} ?? 'rsthn/rose-cli-mods';
             if (!Text::indexOf($url, '//'))
                 $url = 'https://github.com/' . $url . '.git';
 
-            Directory::remove($tmp, true);
+            $tmp = Path::append(Path::fsroot(), 'mod-cache', md5($url));
+            if (Path::exists($tmp)) Directory::remove($tmp, true);
 
-            echo "\x1B[97mFetching repository ...\x1B[0m\n";
-            system ("git clone " . $url . " " . $tmp, $code);
+            system("git clone " . $url . " " . $tmp, $code);
             if ($code) throw new Error('git clone exited with error ' . $code);
 
-            echo "\n\n";
+            echo "\n\e[92mCompleted\e[0m\n";
+            break;
 
-            if ($args->{3})
+        /**
+         * `mod-search`
+         */
+        case 'mod-search':
+            $regex = $args->{2};
+
+            $tmp = Path::append(Path::fsroot(), 'mod-cache');
+            if (!Path::exists($tmp))
+                throw new Error("Module cache not found, run \e[96mmod-pull\e[0m first.");
+
+            foreach (Directory::readDirs ($tmp, false)->dirs->__nativeArray as $dir)
             {
-                $dir = Path::append($tmp, $args->{3});
+                if ($dir->name[0] === '.') continue;
 
-                if (!Path::exists($dir))
-                    throw new Error('Directory ' . $args->{3} . ' not found');
+                foreach (Directory::readDirs($dir->path, false)->dirs->__nativeArray as $mod_dir)
+                {
+                    if ($mod_dir->name[0] === '.') continue;
 
-                echo "\x1B[97mInstalling module `" . $args->{3} . "` ...\x1B[0m";
-                Directory::copy ($dir, Path::append(Path::fsroot(), 'mods', $args->{3}), true, true);
+                    $meta_path = Path::append($mod_dir->path, 'meta.json');
+                    if (!Path::exists($meta_path)) continue;
+
+                    $meta = json_decode(file_get_contents($meta_path), true);
+                    echo "\n  \e[90m" . $mod_dir->name . ":\e[0m \e[97m" . $meta['version'] . "\e[0m";
+                }
+            }
+
+            break;
+
+        /**
+         * `mod-add <mod-name|*> [repo-url]`
+         */
+        case 'mod-add':
+            if (!$args->{2})
+                throw new Error("Parameter \e[97mmod-name\e[0m is missing.");
+
+            $url = $args->{3} ?? 'rsthn/rose-cli-mods';
+            if (!Text::indexOf($url, '//'))
+                $url = 'https://github.com/' . $url . '.git';
+
+            $tmp = Path::append(Path::fsroot(), 'mod-cache', md5($url));
+            if (!Path::exists($tmp))
+                throw new Error("Repository \e[97m" . $url . "\e[0m not in the module cache, run \e[96mmod-pull\e[0m first.");
+    
+            if ($args->get(2) !== '*')
+            {
+                $dir = Path::append($tmp, $args->get(2));
+                if (!Path::exists($dir) || !Path::exists(Path::append($dir, 'meta.json')))
+                    throw new Error("Module source folder \e[97m" . $args->get(2) . "\e[0m not found");
+
+                $meta = json_decode(file_get_contents(Path::append($dir, 'meta.json')), true);
+                if ($meta['preinstall'])
+                {
+                    foreach ($meta['preinstall'] as $cmd) {
+                        system($cmd, $code);
+                        if ($code)
+                            throw new Error("Failed with error code " . $code);
+                    }
+                }
+
+                echo "\nAdding module \e[96m" . $args->get(2) . "\e[0m ...";
+                Directory::copy ($dir, Path::append(Path::fsroot(), 'mods', $args->get(2)), true, true);
             }
             else
             {
                 foreach (Directory::readDirs ($tmp, false)->dirs->__nativeArray as $dir)
                 {
-                    if ($dir->name[0] === '.')
-                        continue;
+                    if ($dir->name[0] === '.') continue;
+                    if (!Path::exists(Path::append($dir->path, 'meta.json'))) continue;
 
-                    echo "\x1B[97mInstalling module `" . $dir->name . "` ...\x1B[0m";
+                    $meta = json_decode(file_get_contents(Path::append($dir->path, 'meta.json')), true);
+                    if ($meta['preinstall'])
+                    {
+                        foreach ($meta['preinstall'] as $cmd) {
+                            system($cmd, $code);
+                            if ($code)
+                                throw new Error("Failed with error code " . $code);
+                        }
+                    }
+    
+                    echo "\nAdding module \e[96m" . $dir->name . "\e[0m ...";
                     Directory::copy ($dir->path, Path::append(Path::fsroot(), 'mods', $dir->name), true, true);
                 }
             }
 
-            Directory::remove($tmp, true);
-
-            echo "\n\x1B[92mCompleted\x1B[0m\n";
+            echo "\nDone.";
             break;
 
-        case 'del':
+        /**
+         * `mod-rm <mod-name>`
+         */
+        case 'mod-rm':
             if (!$args->{2})
-                throw new Error('Parameter <mod-name> is missing.');
+                throw new Error("Parameter \e[97mmod-name\e[0m is missing.");
 
             $path = Path::append(Path::fsroot(), 'mods', $args->{2});
-
             if (!Path::exists($path))
-                throw new Error ("Module \x1B[97m'" . $args->{2} . "'\x1B[0m not installed.");
+                throw new Error ("Module \e[97m'" . $args->{2} . "'\e[0m not installed.");
 
-            echo "\x1B[97mRemoving module `" . $args->{2} . "` ...";
+            echo "Removing module \e[97m" . $args->{2} . "\e[0m ...";
             Directory::remove ($path, true);
-
-            echo "\n\x1B[92mCompleted\x1B[0m\n";
+            echo "\n\e[92mCompleted\e[0m\n";
             break;
 
+        /**
+         * `new [target-folder]`
+         * Default target-folder is "api"
+         */
         case 'new':
             if (!$args->{2}) $args->{2} = 'api';
             system('composer --ansi create-project rsthn/rose-api '.$args->get(2));
             break;
         
         default:
-
             $path = $args->get(1);
-
-            if ($path[0] === ':')
-            {
+            if ($path[0] === ':') {
                 $name = Text::substring($path, 1);
-
                 $path = Path::append(Path::fsroot(), 'mods', $name);
                 if (!Path::exists($path))
-                    throw new Error ("Module \x1B[97m'" . $name . "'\x1B[0m not installed.");
-
+                    throw new Error ("Module \e[97m'" . $name . "'\e[0m not installed.");
                 Rose\Ext\Wind::run($path.'/main.fn', new Map ([ 'args' => $args->slice(2)->unshift(Path::resolve($path.'/main.fn')) ]));
             }
-            else
-            {
+            else {
                 if (Text::endsWith($path, '.fn'))
                     Rose\Ext\Wind::run($path, new Map ([ 'args' => $args->slice(2)->unshift(Path::resolve($path)) ]));
                 else
                     Rose\Ext\Wind::run($path.'.fn', new Map ([ 'args' => $args->slice(2)->unshift(Path::resolve($path.'.fn')) ]));
             }
-
             break;
     }
 }
 catch (Throwable $e) {
-    echo "\x1B[91mError:\x1B[0m " . $e->getMessage() . "\n";
+    echo "\e[91mError:\e[0m " . $e->getMessage() . "\n";
     exit(1);
 }
 
